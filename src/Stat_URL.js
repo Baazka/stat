37c208d8e8a80783f365655f3176ca0335d74278
buyan7 <Bkhishig1@gmail.com>
2023-11-20 17:29:51 +0800

    url

--- src/Stat_URL.js
+++ src/Stat_URL.js
@@ -1,8 +1,8 @@
 const URL = 
-//"http://stat.audit.mn/back/"
+"http://stat.audit.mn/back/"
 //"http://172.16.24.126:3004/";
 //"http://fas.audit.mn/tback/api/v1/";
-"http://localhost:3004/";
+//"http://localhost:3004/";
 //"http://172.16.24.101:3003/api/v1/";
 //"http://172.16.24.126:3004/";
 export default URL;
