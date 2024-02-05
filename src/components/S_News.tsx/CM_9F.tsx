95d8a11ffb35f0ccfc6162f5bbd37a1748cd6cb7
Lhamaa <61314313+Regzlkham@users.noreply.github.com>
2024-02-05 16:23:58 +0800

    CM_9

--- src/components/S_News.tsx/CM_9F.tsx
+++ src/components/S_News.tsx/CM_9F.tsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
-import {
-  useNavigate,
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Title from "../Title";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../../pages/Home.css";
-import Subtitle from "../Subtitle";
-import { excel } from "../../assets/zurag";
 import ButtonSearch from "../ButtonSearch";
+import UserPremission from "../../functions/userPermission";
+import { excel } from "../../assets/zurag";
+import DataRequest from "../../functions/make_Request";
+import Stat_URl from "../../Stat_URL";
 import {
   Column,
   Table,
@@ -20,19 +17,15 @@ import {
   getFacetedUniqueValues,
   getFacetedMinMaxValues,
   getPaginationRowModel,
-  sortingFns,
   getSortedRowModel,
   FilterFn,
-  SortingFn,
   ColumnDef,
   flexRender,
-  FilterFns,
 } from "@tanstack/react-table";
 
 import {
   RankingInfo,
   rankItem,
-  compareItems,
 } from "@tanstack/match-sorter-utils";
 declare module "@tanstack/table-core" {
   interface FilterFns {
@@ -54,41 +47,52 @@ const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
   // Return if the item should be filtered in/out
   return itemRank.passed;
 };
-const now = new Date();
-
-type Stat_CM9F = {
-  AUDIT_GAZAR: string;
-  AUDIT_KOD: string;
-  AUDIT_HH_NEGJ_NR: string;
-  ADUIT_HIIH_BAI_NER: string;
-  BAI_NER: string;
-  REGISTER: string;
-  ZOWLOMJ_OGON_HUGATSAA: string;
-  HSHU_HIISEN_ARGA_HEMJEE: string;
-  ZOWLOMJIIN_UTGA: string;
-  ZOWLOMJIIN_MEDEELLN_TOROL: string;
-  ZOWLOMJIIN_BIELSEN_ESEH: string;
-  ZOWLOMJIIN_HUGATSAA: string;
-  SHIIDWERLEGDSEN_ORGDOL_GOMDLN_TOO: string;
-  ZOWLOMJIIN_HERGJEEGUI_SHALTGAAN_TAILBAR: string;
-};
 
 function CM_9F() {
+  // @ts-ignore
+  const userDetails = JSON.parse(localStorage.getItem("userDetails"));
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
   );
-  // const userDetails = JSON.parse(getItem("userDetails"));
-  // const [status, setStatus] = useState([]);
-  // const [commentText, setCommentText] = useState("");
-  // async function fetchData() {
   const [globalFilter, setGlobalFilter] = React.useState("");
 
-  const columns = React.useMemo<ColumnDef<Stat_CM9F, any>[]>(
+  const columns = React.useMemo(
     () => [
-      {
-        accessorFn: (row, index) => index + 1,
-        id: "№",
-      },
+      UserPremission(userDetails.USER_TYPE_NAME, "plan", "lock")
+      ? {
+          id: "select",
+          header: ({ table }) => (
+            <IndeterminateCheckboxALL
+              {...{
+                checked: table.getIsAllRowsSelected(),
+                indeterminate: table.getIsSomeRowsSelected(),
+                onChange: table.getToggleAllRowsSelectedHandler(),
+                data,
+                setData,
+              }}
+            />
+          ),
+          cell: ({ row }) => (
+            <div>
+              <IndeterminateCheckbox
+                {...{
+                  checked: row.original.IS_LOCK === 1 ? true : false, //ow.getIsSelected(), //row.IS_LOCK === 1 ?true:false
+                  disabled: !row.getCanSelect(),
+                  indeterminate: row.getIsSomeSelected(),
+                  onChange: row.getToggleSelectedHandler(),
+                  data,
+                  setData,
+                  row,
+                }}
+              />
+            </div>
+          ),
+        }
+      : {
+          accessorFn: (row, index) => index + 1,
+          accessorKey: "№",
+          header: "№",
+        },
       {
         accessorKey: "AUDIT_GAZAR",
         cell: (info) => info.getValue(),
@@ -164,8 +168,8 @@ function CM_9F() {
     ],
     []
   );
-  let Stat_CM9F = [{}];
-  const [data, setData] = React.useState<Stat_CM9F[]>(Stat_CM9F);
+
+  const [data, setData] = React.useState([]);
   const Navigate = useNavigate();
   const refreshData = () => setData((old) => []);
   const [filter, setFilter] = useState({
@@ -206,6 +210,30 @@ function CM_9F() {
     debugColumns: false,
   });
 
+  useEffect(() => {
+    fetchData();
+  }, [filter]);
+
+  async function fetchData() {
+    DataRequest({
+      url: Stat_URl + "statisticList",
+      method: "POST",
+      data: {
+        
+      },
+    })
+      .then(function (response) {
+        if (response?.data !== undefined && response?.data?.length > 0) {
+          setData([]);
+        } else {
+          setData([]);
+        }
+      })
+      .catch(function (error) {
+        alert("Өгөгдөл авчрахад алдаа гарлаа!");
+      });
+  }
+
   React.useEffect(() => {
     if (table.getState().columnFilters[0]?.id === "fullName") {
       if (table.getState().sorting[0]?.id !== "fullName") {
@@ -220,22 +248,10 @@ function CM_9F() {
         style={{
           maxHeight: window.innerHeight - 129,
           maxWidth: window.innerWidth,
-          padding: "0.5rem 0 0 0.5rem",
+          padding: "0.5rem 0 0 1rem",
           overflowX: "scroll",
         }}
       >
-        <div className="justify-start flex mb-2 mt-2">
-          <Title
-            title={
-              "ТӨРИЙН АУДИТЫН БАЙГУУЛЛАГЫН МЭДЭЭЛЛИЙН ИЛ ТОД БАЙДАЛ 3-ТАБСМ-9Е"
-            }
-            widthS={"40rem"}
-            widthL={"11rem"}
-          />
-          <div className="mt-1 ml-1.5">
-            <Subtitle mayagtName={"З-ТАБСМ-9Е"} />
-          </div>
-        </div>
         <div className="flex justify-between mb-2 ">
           <div style={{ height: 28 }} className="flex flex-row  cursor-pointer">
             <ButtonSearch />
@@ -272,77 +288,75 @@ function CM_9F() {
           }}
         >
           <div className="h-2 mr-20" />
-          <table>
-            <thead className="TableHeadBackroundcolor gap-20">
-              {table.getHeaderGroups().map((headerGroup) => (
-                <tr key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <th
-                        key={header.id}
-                        colSpan={header.colSpan}
-                        style={{
-                          width:
-                            header.getSize() !== 0
-                              ? header.getSize()
-                              : undefined,
-                        }}
-                      >
-                        {header.isPlaceholder ? null : (
-                          <>
-                            <div
-                              onMouseDown={header.getResizeHandler()}
-                              onTouchStart={header.getResizeHandler()}
-                            ></div>
-                            <div
-                              {...{
-                                className: header.column.getCanSort()
-                                  ? "cursor-pointer select-none"
-                                  : "",
-                                onClick:
-                                  header.column.getToggleSortingHandler(),
-                              }}
-                            >
-                              {flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                              )}
+          <table className="w-full">
+          <thead className="TableHeadBackroundcolor gap-20">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th
+                      key={header.id}
+                      colSpan={header.colSpan}
+                      className="px-1.5"
+                      style={{
+                        width:
+                          header.getSize() !== 0 ? header.getSize() : undefined,
+                      }}
+                    >
+                      {header.isPlaceholder ? null : (
+                        <>
+                          <div
+                            onMouseDown={header.getResizeHandler()}
+                            onTouchStart={header.getResizeHandler()}
+                          ></div>
+                          <div
+                            {...{
+                              className: header.column.getCanSort()
+                                ? "cursor-pointer select-none"
+                                : "",
+                              onClick: header.column.getToggleSortingHandler(),
+                            }}
+                          >
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                          </div>
+                          {header.column.getCanFilter() ? (
+                            <div>
+                              <Filter column={header.column} table={table} />
                             </div>
-                            {header.column.getCanFilter() ? (
-                              <div>
-                                <Filter column={header.column} table={table} />
-                              </div>
-                            ) : null}
-                          </>
+                          ) : null}
+                        </>
+                      )}
+                    </th>
+                  );
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row, i) => {
+              return (
+                <tr
+                  key={row.id}
+                  className={i % 2 > 0 ? "tr bg-gray-100" : "tr"}
+                >
+                  {row.getVisibleCells().map((cell, index) => {
+                    return (
+                      <td key={cell.id} className="p-2">
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
                         )}
-                      </th>
+                      </td>
                     );
                   })}
                 </tr>
-              ))}
-            </thead>
-            <tbody>
-              {table.getRowModel().rows.map((row, i) => {
-                return (
-                  <tr
-                    key={row.id}
-                    className={i % 2 > 0 ? "tr bg-gray-100" : "tr"}
-                  >
-                    {row.getVisibleCells().map((cell, index) => {
-                      return (
-                        <td key={cell.id} className="p-2">
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext()
-                          )}
-                        </td>
-                      );
-                    })}
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+              );
+            })}
+          </tbody>
+        </table>
         </div>
       </div>
       <div style={{ justifyContent: "flex-end" }}>
@@ -375,28 +389,109 @@ function CM_9F() {
           >
             {">>"}
           </button>
-          <span className="flex items-center gap-4">
-            <div>нийт</div>
-            <strong>
-              {table.getState().pagination.pageIndex + 1} {table.getPageCount()}
-            </strong>
-          </span>
-          <select
-            value={table.getState().pagination.pageSize}
-            onChange={(e) => {
-              table.setPageSize(Number(e.target.value));
-            }}
-            className="border p-0.8 bg-blue-300 rounded-lg text-white ml-2"
-          >
-            {[10, 20, 30, 40, 50].map((pageSize) => (
-              <option key={pageSize} value={pageSize}>
-                {pageSize}
-              </option>
-            ))}
-          </select>
-        </div>
+            <span className="flex items-center gap-4">
+              <div>нийт:</div>
+              <span>{data.length}</span>
+              <strong>
+                {table.getState().pagination.pageIndex + 1}
+                {" - "}
+                {table.getPageCount()}
+              </strong>
+              </span>
+              <select
+                value={table.getState().pagination.pageSize}
+                onChange={(e) => {
+                  table.setPageSize(Number(e.target.value));
+                }}
+                className="border p-0.8 bg-blue-300 rounded-lg text-white ml-2"
+              >
+                {[10, 20, 30, 40, 50].map((pageSize) => (
+                  <option key={pageSize} value={pageSize}>
+                    {pageSize}
+                  </option>
+                ))}
+              </select>
+          </div>
       </div>
     </>
+  );
+}
+
+function IndeterminateCheckbox({
+  indeterminate,
+  className = "",
+  data,
+  setData,
+  row,
+
+  ...rest
+}: { indeterminate?: boolean } & HTMLProps<HTMLInputElement>) {
+  const ref = React.useRef<HTMLInputElement>(null!);
+  // @ts-ignore
+  const userDetails = JSON.parse(localStorage.getItem("userDetails"));
+  React.useEffect(() => {
+    if (typeof indeterminate === "boolean") {
+      ref.current.indeterminate = !rest.checked && indeterminate;
+    }
+  }, [ref, indeterminate]);
+
+  function saveToDB(value) {
+    let tempData = data;
+    tempData[row.index].IS_LOCK = row.original.IS_LOCK === 0 ? 1 : 0;
+    setData(tempData);
+  }
+
+  return (
+    <input
+      type="checkbox"
+      ref={ref}
+      className={className + " cursor-pointer"}
+      //{row?.original.IS_LOCK === 1 ?true:false}
+      checked={true}
+      onClick={(value) => saveToDB(ref)}
+      {...rest}
+    />
+  );
+}
+
+function IndeterminateCheckboxALL({
+  indeterminate,
+  className = "",
+  data,
+  setData,
+
+  table,
+  ...rest
+}: { indeterminate?: boolean } & HTMLProps<HTMLInputElement>) {
+  const ref = React.useRef<HTMLInputElement>(null!);
+  // @ts-ignore
+  const userDetails = JSON.parse(localStorage.getItem("userDetails"));
+  React.useEffect(() => {
+    if (typeof indeterminate === "boolean") {
+      ref.current.indeterminate = !rest.checked && indeterminate;
+    }
+  }, [ref, indeterminate]);
+
+  function saveToDB() {
+    let tempData = data;
+    for (let i = 0; i < data.length; i++) {
+      tempData[i].IS_LOCK = data[i].IS_LOCK === 0 ? 1 : 0;
+      if (i === data.length - 1) {
+        setData(tempData);
+      }
+    }
+  }
+
+  return (
+    <input
+      type="checkbox"
+      ref={ref}
+      className={className + " cursor-pointer"}
+      //{row?.original.IS_LOCK === 1 ?true:false}
+      checked={true}
+      onClick={(value) => saveToDB()}
+      {...rest}
+    />
   );
 }
 
@@ -435,63 +530,6 @@ function Filter({
 
       <div className="h-4" />
     </>
-  );
-}
-
-// A debounced input react component
-function DebouncedInput({
-  value: initialValue,
-  onChange,
-  debounce = 500,
-  ...props
-}: {
-  value: string | number;
-  onChange: (value: string | number) => void;
-  debounce?: number;
-} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange">) {
-  const [value, setValue] = React.useState(initialValue);
-
-  React.useEffect(() => {
-    setValue(initialValue);
-  }, [initialValue]);
-
-  React.useEffect(() => {
-    const timeout = setTimeout(() => {
-      onChange(value);
-    }, debounce);
-
-    return () => clearTimeout(timeout);
-  }, [value]);
-
-  return (
-    <div className=" overflow-hidden flex border rounded-md">
-      <input
-        type="text"
-        value={value || ""}
-        className=" text-sm "
-        onChange={(e) => {
-          setValue(e.target.value);
-          onChange(e.target.value);
-        }}
-        placeholder="Хайх утгаа оруулна уу..."
-        style={{
-          width: "200px",
-        }}
-      />
-
-      <button className="flex items-center px-2.5 border-l bg-blue-500 rounded-md">
-        <svg
-          className="h-4 w-4 text-grey-dark"
-          fill="currentColor"
-          color="white"
-          enableBackground=""
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
-        </svg>
-      </button>
-    </div>
   );
 }
 
