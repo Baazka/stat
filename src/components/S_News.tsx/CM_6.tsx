3e96c34e938ea524432740fa94db73017a5440a7
Lhamaa <61314313+Regzlkham@users.noreply.github.com>
2024-02-05 16:51:10 +0800

    Update CM_6.tsx

--- src/components/S_News.tsx/CM_6.tsx
+++ src/components/S_News.tsx/CM_6.tsx
@@ -92,6 +92,7 @@ function CM_6() {
           accessorFn: (row, index) => index + 1,
           accessorKey: "№",
           header: "№",
+          size: 10,
         },
       {
         accessorKey: "MD",
