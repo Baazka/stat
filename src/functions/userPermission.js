1efadb7d4272006b039d7b95149ac0c522ae6938
baazka <baazka0705@gmail.com>
2024-02-27 17:51:28 +0800

    -anuzg

--- src/functions/userPermission.js
+++ src/functions/userPermission.js
@@ -1,9 +1,9 @@
 import { checkNullInt } from "./Tools";
-function UserPremission(userType, scope,action) {
-  if(scope === "mayagt"){
-    if(action === 'confirm'){
-      switch(checkNullInt(userType)) {
-       case 2:
+function UserPremission(userType, scope, action) {
+  if (scope === "mayagt") {
+    if (action === "confirm") {
+      switch (checkNullInt(userType)) {
+        case 2:
           return true;
         case 3:
           return true;
@@ -12,51 +12,51 @@ function UserPremission(userType, scope,action) {
         default:
           return false;
       }
-    }else if(action === 'save'){
-      switch(checkNullInt(userType)) {
+    } else if (action === "save") {
+      switch (checkNullInt(userType)) {
         case 1:
           return true;
         default:
           return false;
       }
     }
-  }else if(scope === 'plan'){
-    if(action === 'view'){
-      if(userType.toUpperCase() === 'admin'.toUpperCase()){
+  } else if (scope === "plan") {
+    if (action === "view") {
+      if (userType.toUpperCase() === "admin".toUpperCase()) {
         return true;
-      }else if(userType.toUpperCase() === 'STAT_ADMIN'.toUpperCase()){
+      } else if (userType.toUpperCase() === "STAT_ADMIN".toUpperCase()) {
         return true;
-      }else if(userType.toUpperCase() === 'HEAD_AUDITOR'.toUpperCase()){
-        return false
-      }else if(userType.toUpperCase() === 'MANAGER'.toUpperCase()){
-        return false
-      }else if(userType.toUpperCase() === 'AUDITOR'.toUpperCase()){
-        return false
-      }else if(userType.toUpperCase() === 'ALL_VIEWER'.toUpperCase()){
-        return true
-      }else if(userType.toUpperCase() === 'VIEWER'.toUpperCase()){
-        return false
+      } else if (userType.toUpperCase() === "HEAD_AUDITOR".toUpperCase()) {
+        return false;
+      } else if (userType.toUpperCase() === "MANAGER".toUpperCase()) {
+        return false;
+      } else if (userType.toUpperCase() === "AUDITOR".toUpperCase()) {
+        return false;
+      } else if (userType.toUpperCase() === "ALL_VIEWER".toUpperCase()) {
+        return true;
+      } else if (userType.toUpperCase() === "ANUZG".toUpperCase()) {
+        return true;
+      } else if (userType.toUpperCase() === "VIEWER".toUpperCase()) {
+        return false;
       }
-    }else if (action === 'write') {
-      switch(userType.toUpperCase()) {
-        case 'admin'.toUpperCase():
+    } else if (action === "write") {
+      switch (userType.toUpperCase()) {
+        case "admin".toUpperCase():
           return true;
-          
-        case 'MANAGER'.toUpperCase():
+
+        case "MANAGER".toUpperCase():
           return true;
         default:
           return false;
       }
-
-    }else if(action === 'lock') {
-      if(userType === 'STAT_ADMIN'){
-        return true
-      }else{
+    } else if (action === "lock") {
+      if (userType === "STAT_ADMIN") {
+        return true;
+      } else {
         return false;
       }
     }
   }
-    
 }
 
 export default UserPremission;
